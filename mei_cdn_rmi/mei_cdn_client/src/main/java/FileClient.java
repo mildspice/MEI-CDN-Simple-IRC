93ab40adb2260fe71977af26889b1999fd9b6097
SterbenCyrodiil <rodrigo.coelho1999@gmail.com>
2021-12-16 10:41:00 +0000

    Added Upload and Download in client

--- mei_cdn_rmi/mei_cdn_client/src/main/java/FileClient.java
+++ mei_cdn_rmi/mei_cdn_client/src/main/java/FileClient.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
@@ -13,26 +14,43 @@ public class FileClient extends UnicastRemoteObject implements FileClientInterfa
 
     }
 
-    public byte[] downloadFileFromServer(FileServerInterface fServer, String path) throws RemoteException {
-
-        return null;
+    public boolean downloadFileFromServer(FileServerInterface fServer, String path) throws RemoteException {
+        byte [] data = fServer.downloadFileFromServer(path);
+        File clientpathfile = new File(path);
+        FileOutputStream out;
+        try {
+            out = new FileOutputStream(clientpathfile);
+            out.write(data);
+            out.flush();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }				
+       
+        return true;
     }
 
-    public boolean uploadFileToServer(FileServerInterface fServer, String path) throws RemoteException {
-        try{
+    public byte[] uploadFileToServer(FileServerInterface fServer, String path) throws RemoteException {
         File clientpathfile = new File(path);
-        byte [] mydata=new byte[(int) clientpathfile.length()];
+        byte [] data=new byte[(int) clientpathfile.length()];
+        try{
         FileInputStream in=new FileInputStream(clientpathfile);		
-         in.read(mydata, 0, mydata.length);					 
-         fServer.uploadFileToServer(mydata, path, (int) clientpathfile.length());        
+         in.read(data, 0, data.length);					 
+         fServer.uploadFileToServer(data, path, (int) clientpathfile.length());        
          in.close();
         }catch (FileNotFoundException e) {     
             e.printStackTrace();
+            return null;
         }	
         catch (IOException e) {        
             e.printStackTrace();
+            return null;
         }	
-        return false;
+        return data;
     }
     
 }
--- mei_cdn_rmi/mei_cdn_rmi_interfaces/src/main/java/FileClientInterface.java
+++ mei_cdn_rmi/mei_cdn_rmi_interfaces/src/main/java/FileClientInterface.java
@@ -2,7 +2,7 @@ import java.rmi.RemoteException;
 
 public interface FileClientInterface {
 
-    public boolean uploadFileToServer(FileServerInterface fServer, String path) throws RemoteException;
-	public byte[] downloadFileFromServer(FileServerInterface fServer, String path) throws RemoteException;
+    public byte[] uploadFileToServer(FileServerInterface fServer, String path) throws RemoteException;
+	public boolean downloadFileFromServer(FileServerInterface fServer, String path) throws RemoteException;
 
 }
